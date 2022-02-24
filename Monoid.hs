27ed55c0f54d4b5322b3d83b1de3896908566c83
nomyfan <Nomyfan@live.com>
2022-02-24 23:30:31 +0800

    Monoid day 2

--- Monoid.hs
+++ Monoid.hs
@@ -1,3 +1,6 @@
+import qualified Data.Foldable as F
+import           Data.Monoid
+
 class MyMonoid m where
   mmempty :: m
   mmappend :: m -> m -> m
@@ -16,3 +19,11 @@ newtype MyProduct a  = MyProduct { getProduct :: a }
 instance Num a => MyMonoid (MyProduct a) where
   mmempty = MyProduct 1
   MyProduct x `mmappend` MyProduct y = MyProduct (x * y)
+
+-- 类似“短路”，我们应该把更重要的比较放在前边。
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = (length x `compare` length y) `mappend`
+  (x `compare` y)
+
+-- F.foldl (+) 2 (Just 9)
+-- F.foldr (||) False (Just True)
