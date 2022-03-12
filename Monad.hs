71fb3fc721d75bcea6bf4344ba8e3f6eae8bbcb9
nomyfan <Nomyfan@live.com>
2022-03-12 14:15:05 +0800

    Monad day 3

--- Monad.hs
+++ Monad.hs
@@ -1,3 +1,4 @@
+import           Control.Monad (MonadPlus (mzero), guard)
 applyMaybe :: Maybe a -> (a -> Maybe b)  -> Maybe b
 applyMaybe Nothing f  = Nothing
 applyMaybe (Just x) f = f x
@@ -60,3 +61,47 @@ withDoNotation = do
   Just (show x ++ y)
 
 -- 当do块里pattern matching失败，那么会调用fail方法
+
+--
+myGuard :: (MonadPlus m) => Bool -> m ()
+myGuard True  = return ()
+myGuard False = mzero
+
+-- [1..50] >>= (\x -> myGuard ('7' `elem` show x) >> return x)
+-- Written in do notation
+sevensOnly :: [Int]
+sevensOnly = do
+  x <- [1..50]
+  -- 以下代码都是>>=之后的
+  guard ('7' `elem` show x)
+  return x
+
+-- A knight's quest
+type KnightPos = (Int, Int)
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c,r) = do
+  (c',r') <- [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
+              ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)]
+  guard (c' `elem` [1..8] && r' `elem` [1..8])
+  return (c', r')
+
+moveKnightUsingFilter :: KnightPos -> [KnightPos]
+moveKnightUsingFilter (c,r) = filter onBoard
+  [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
+  ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)]
+  where onBoard (c,r) = c `elem` [1..8] && r `elem` [1..8]
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = pure start >>= moveKnight >>= moveKnight >>= moveKnight
+-- in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+-- ↓ Left identity(from Monad Raws)
+-- in3 start = moveKnight start >>= moveKnight >>= moveKnight
+-- in3 start = [start] >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
+
+-- Monad raws
+-- Left identity: return m >>= f SAME_AS f m
+-- Right identity: m >>= return SAME_AS m
+-- Associativity: (m >>= f) >>= g SAME_AS m >>= (\x -> f x >>= g)
